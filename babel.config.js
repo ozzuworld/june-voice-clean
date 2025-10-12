9d6cdea76014583772a1adc3f7b14ea0a2151a6a
ozzuworld <eng.ozzu@icloud.com>
2025-10-12 11:30:55 -0500

    Move babel.config.js from apps/expo to root

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            '@/components': './components',
+            '@/hooks': './hooks',
+            '@/services': './services',
+            '@/types': './types',
+            '@/config': './config',
+            '@/utils': './utils',
+            '@/constants': './constants',
+          },
+        },
+      ],
+      // React Native Reanimated plugin must be listed last
+      'react-native-reanimated/plugin',
+    ],
+  };
+};

4d45ae1495cfa4cda998ce2d756f29bd5adc3949
Kazuma-PC <mail@ozzu.world>
2025-10-16 18:10:58 -0500

    1

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,6 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // expo-router/babel is now included in babel-preset-expo
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-          },
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
-  };
+api.cache(true);
+return {
+presets: ['babel-preset-expo'],
+};
 };

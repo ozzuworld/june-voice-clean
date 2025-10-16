a7d644c831761f596ba32d855e241fad8cf1006e
ozzuworld <eng.ozzu@icloud.com>
2025-10-16 18:12:59 -0500

    Add TextEncoder/TextDecoder polyfills for Hermes in index.js and keep LiveKit registerGlobals.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// index.js - minimal entry for LiveKit RN per official quickstart
+// index.js - minimal entry with essential Hermes polyfills for LiveKit
+import 'react-native-get-random-values';
+import { TextEncoder, TextDecoder } from 'text-encoding';
 import { registerGlobals } from '@livekit/react-native';
 import { registerRootComponent } from 'expo';
 import App from './App';
+
+// Essential polyfills for Hermes
+if (typeof global.TextEncoder === 'undefined') {
+  // @ts-ignore
+  global.TextEncoder = TextEncoder;
+}
+if (typeof global.TextDecoder === 'undefined') {
+  // @ts-ignore
+  global.TextDecoder = TextDecoder;
+}
 
 // Register LiveKit global WebRTC shims before anything else
 registerGlobals();
